8df9514b66c1ca43f9fbd380d03d845773102d34
hata <wmf8341@gmail.com>
2021-10-09 13:04:43 +0900

    api-url

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,5 @@
 const ENV = require('dotenv').config().parsed;
+const { NODE_ENV, API_URL } = process.env;
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
@@ -62,9 +63,14 @@ export default {
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  env:ENV,
+  env: {
+    NODE_ENV,
+    API_URL,
+    ENV
+  },
   axios: {
-    baseURL:ENV.API_BASE_URL,
+    // baseURL:ENV.API_BASE_URL,
+    baseURL:process.env.API_URL,
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
