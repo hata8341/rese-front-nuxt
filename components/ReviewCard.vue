1e1b99339849c5180b6f31bf44bc781957c15b6e
hata <wmf8341@gmail.com>
2022-07-14 16:35:13 +0900

    レビュー削除アイコンの修正

--- components/ReviewCard.vue
+++ components/ReviewCard.vue
@@ -1,36 +1,28 @@
 <template>
-<div>
-  <div class="review-card"
-  v-for="(review,index) in reviews"
-  :key="index">
-    <div class="review-user-wrap">
-      <h3 class="review-user">{{review.user.user_name}}さん</h3>
-      <font-awesome-icon icon="times-circle" class="times-circle"
-      v-if="$auth.user.id === review.user_id"
-      @click="deleteReview(review.id,index)"></font-awesome-icon>
-    </div>
-    <div class="review-stars-wrap">
-      <star-rating
-          class="review-stars-input"
-          :max-rating="5"
-          :rating="review.stars"
-          inactive-color="#000"
-          active-color="#e64c30"
-          :star-size="25"
-          :read-only="true"
-          >
+  <div>
+    <div class="review-card" v-for="(review,index) in reviews" :key="index">
+      <div class="review-user-wrap">
+        <h3 class="review-user">{{review.user.user_name}}さん</h3>
+        <div class="icon-wrap">
+          <font-awesome-icon icon="times-circle" class="times-circle" v-if="$auth.user.id === review.user_id"
+          @click="deleteReview(review.id,index)"></font-awesome-icon>
+        </div>
+      </div>
+      <div class="review-stars-wrap">
+        <star-rating class="review-stars-input" :max-rating="5" :rating="review.stars" inactive-color="#000"
+          active-color="#e64c30" :star-size="25" :read-only="true">
         </star-rating>
-    </div>
-    <div class="review-title-wrap">
-      <h3 class="review-title">{{review.review_title}}</h3>
-    </div>
-    <div class="review-comment-wrap">
-      <p class="review-comment">
-        {{review.comment}}
-      </p>
+      </div>
+      <div class="review-title-wrap">
+        <h3 class="review-title">{{review.review_title}}</h3>
+      </div>
+      <div class="review-comment-wrap">
+        <p class="review-comment">
+          {{review.comment}}
+        </p>
+      </div>
     </div>
   </div>
-</div>
 </template>
 
 
@@ -91,6 +83,12 @@ export default {
 .review-user-wrap {
   display: flex;
   justify-content: space-between;
+  align-items: center;
+}
+.icon-wrap {
+  width: 22px;
+  height: 22px;
+  text-align: center;
 }
 .times-circle {
   font-size: 22px;
